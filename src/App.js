5704ddbbe552df7737e458ab015b74d09017cb32
Nicholas Renotte <nicholas.renotte@ibm.com>
2020-11-01 22:43:13 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+// 1. Install dependencies DONE
+// 2. Import dependencies DONE
+// 3. Setup webcam and canvas DONE
+// 4. Define references to those DONE
+// 5. Load posenet DONE
+// 6. Detect function DONE
+// 7. Drawing utilities from tensorflow DONE
+// 8. Draw functions DONE
+
+import React, { useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
+import image from "./img.jpg";
+
+function App() {
+  const canvasRef = useRef(null);
+  const imgRef = useRef(null);
+
+  //  Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+    });
+    //
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof imgRef.current !== "undefined" &&
+      imgRef.current !== null
+    ) {
+      // Get Video Properties
+      const video = imgRef.current;
+      const videoWidth = imgRef.current.width;
+      const videoHeight = imgRef.current.height;
+      console.log(videoHeight);
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.6, ctx);
+    drawSkeleton(pose["keypoints"], 0.8, ctx);
+  };
+
+  runPosenet();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img ref={imgRef} src={image} />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 960,
+            height: 689,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
